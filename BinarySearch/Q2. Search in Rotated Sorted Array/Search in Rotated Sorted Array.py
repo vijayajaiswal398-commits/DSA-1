c205246d0108a37a1d916020a68feedfb1611eea
Vijaya Jaiswal <vijayajaiswal398@gmail.com>
2026-01-22 20:59:42 +0530

    Add search function for rotated sorted array
    Implement binary search in a rotated sorted array.

---
+++ BinarySearch/Q2. Search in Rotated Sorted Array/Search in Rotated Sorted Array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return -1
+#starting from the start and end point of the array searching for target b/w left & mid or mid & right. in the array as given array is rotated sorted array .there are two rotated sorted arr in a single arr.
